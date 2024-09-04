683a3820d49b535a26325945a04fc62df7820df3
ELMURODSALOMOV <salomovelmurod062@gmail.com>
2024-09-04 11:54:09 +0500

    BROKERS: Insert Student

--- StudentCRUD.Core.Api/Brokers/Storages/IStorageBroker.Student.cs
+++ StudentCRUD.Core.Api/Brokers/Storages/IStorageBroker.Student.cs
@@ -1,6 +1,9 @@
-﻿namespace StudentCRUD.Core.Api.Brokers.Storages
+﻿using StudentCRUD.Core.Api.Models.Students;
+
+namespace StudentCRUD.Core.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        ValueTask<Student> InsertStudentAsync(Student student);
     }
 }
